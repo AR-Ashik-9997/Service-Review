d7a448a386fe77ca6caae0b038ba743d75f5ab75
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2022-11-07 22:58:47 +0600

    Navigation bar is created and folder structuring is created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to='/' className="fs-2">Courier Service</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to='/'>Home</Nav.Link>
+            <Nav.Link as={Link} to='/sign-in'>Sign-In</Nav.Link>            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
---
+++ src/utility/Routes.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home/Home";
+import Login from "../components/Login/Login";
+import Register from "../components/Register/Register";
+import Root from "./Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/sign-in", element: <Login /> },
+      { path: "/register", element: <Register /> },
+    ],
+  },
+]);
+
+export default router;
